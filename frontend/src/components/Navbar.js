ad6429ff6c1bbce6e228ba16be741fb074088af1
Mart Anthony Salazar <martsalazar0@gmail.com>
2022-10-16 00:31:22 +0800

    add links

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,16 +1,19 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
   return (
     <header>
       <div className="container">
         <Link to="/">
           <h1>Workout Buddy</h1>
         </Link>
+        <nav>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+        </nav>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
