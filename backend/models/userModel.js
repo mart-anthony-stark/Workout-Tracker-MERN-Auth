690f7769e9199c183dc688079070658bc7ac3b7e
Mart Anthony Salazar <martsalazar0@gmail.com>
2022-10-15 22:04:58 +0800

    add hashing passwords

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
   email: {
@@ -11,5 +12,21 @@ const UserSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+// static signup method
+UserSchema.statics.signup = async function (email, password) {
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error("Email already in use");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
 
 module.exports = mongoose.model("User", UserSchema);
