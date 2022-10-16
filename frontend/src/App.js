2bfe4a29f3b8e8e58affa03f709b3a293c58a250
Mart Anthony Salazar <martsalazar0@gmail.com>
2022-10-16 13:54:17 +0800

    add route protections

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,29 @@ import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
+import { useAuthContext } from "./hooks/useAuthContext";
+import { Navigate } from "react-router-dom";
 
 function App() {
+  const { user } = useAuthContext();
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
+            <Route
+              path="/"
+              element={!user ? <Navigate to="/login" /> : <Home />}
+            />
+            <Route
+              path="/login"
+              element={user ? <Navigate to="/" /> : <Login />}
+            />
+            <Route
+              path="/signup"
+              element={user ? <Navigate to="/" /> : <Signup />}
+            />
           </Routes>
         </div>
       </BrowserRouter>
