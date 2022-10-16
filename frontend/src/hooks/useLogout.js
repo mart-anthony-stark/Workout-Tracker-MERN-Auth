fbd2b6b22ef729757985e5cfe8bd17b313c51f85
Mart Anthony Salazar <martsalazar0@gmail.com>
2022-10-16 14:19:11 +0800

    add clear workouts global state

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,15 +1,18 @@
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "./useAuthContext";
+import { useWorkoutsContext } from "./useWorkoutsContext";
 
 export const useLogout = () => {
   const navigate = useNavigate();
   const { dispatch } = useAuthContext();
+  const { dispatch: workoutsDispatch } = useWorkoutsContext();
   const logout = () => {
     // remove user from storage
     localStorage.removeItem("user");
 
     // dispatch logout action
     dispatch({ type: "LOGOUT" });
+    workoutsDispatch({ type: "SET_WORKOUTS", payload: null });
     navigate("/login");
   };
 
